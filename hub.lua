ef85795d32b9ad2044ef7f774e8816cc829e65c0
celer-lab <lupitkog68@gmail.com>
2024-07-30 19:51:40 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("CelestialHub", "Sentinel")
+local Window = Library.CreateLib("CelestialHub", "Midnight")
 local Tab = Window:NewTab("MM2")
 local Section = Tab:NewSection("MM2")
 Section:NewButton("MM2", "MM2", function()
